0ff51d444643e31a690bb899ddb9c31fbc8f8d96
Joachim Bellet <jobellet@gmail.com>
2025-05-17 08:20:00 +0200

    '

--- utils/extract_and_download_data.py
+++ utils/extract_and_download_data.py
@@ -1,3 +1,8 @@
+import urllib.request
+import zipfile
+import shutil
+import os
+
 def download_figshare_file(code, filename,private_link = '',force_download = False):
     """
     Download a file from a private Figshare link if not present locally.
@@ -6,9 +11,15 @@ def download_figshare_file(code, filename,private_link = '',force_download = Fal
     if len(private_link)>0:
         link = f'https://figshare.com/ndownloader/files/{code}?private_link={private_link}'
     else:
-        link = f'https://figshare.com/ndownloader/files/{code}
+        link = f'https://figshare.com/ndownloader/files/{code}'
     if (not os.path.exists(filename)) or force_download:
         print(f"Downloading {filename}...")
         urllib.request.urlretrieve(link, filename)
     else:
         print(f"{filename} already exists. Skipping download.")
+
+def unzip(zip_path,extract_path):
+    # create a ZipFile object
+    with zipfile.ZipFile(zip_path, 'r') as zip_ref:
+        # extract all the files in the zip archive to the specified directory
+        zip_ref.extractall(extract_path)
